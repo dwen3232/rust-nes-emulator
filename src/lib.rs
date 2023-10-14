ed0a5b00b07119da0a3aece2955d845e8aeb1a9c
dwen3232 <davidrwen@gmail.com>
2023-10-14 17:46:36 -0400

    creating freshish branch for refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code, unused, unused_imports)]
 
-pub mod cpu;
-pub mod ppu;
-pub mod cartridge;
-pub mod screen;
-pub mod trace;
-pub mod traits;
-pub mod controller;
+// pub mod cpu;
+// pub mod ppu;
+// pub mod cartridge;
+// pub mod screen;
+// pub mod trace;
+// pub mod traits;
+// pub mod controller;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use std::env;
 
-use rust_nes_emulator::screen::run;
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if let Some(path) = args.get(1) {
-        run(path);
-    } else {
-        println!("Pass .nes file path to run")
-    }
+    println!("Ran")
 }
